75513fbbb82a27c62cfa481ea2f8203d65145b1b
bibekboi <bibek.rizal1@gmail.com>
2023-03-08 09:57:01 +0545

    Initiated Player Movement

---
+++ SherpaClimb/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Active");    
+    }
+}
